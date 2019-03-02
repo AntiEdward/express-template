38ac8d5a2b7fcb876fa4e0169d64a617cbda82be
Edward <hikaru1992@126.com>
2019-03-02 17:04:25 +0800

    commit

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('pug', require('pug').__express) //没有这句会报错 找不到pug
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -17,11 +19,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

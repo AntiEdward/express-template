24539dc14d133127e523ccba2affe30b1873d0cd
Edward <hikaru1992@126.com>
2019-03-05 02:14:16 +0800

    commit

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// 定义public下的路由
 app.use('/public', express.static(path.join(__dirname, 'public')));
+// 定义dist下的路由，默认路径
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/', indexRouter);
